63d49e5fc05c0358c2fa6e45d4e9606cd2bb6bd0
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-10-07 20:48:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-
 import '../styles/App.css';
 import React, { useState } from 'react';
 
 export default function App() {
  //code here 
-
+ let [inp,setInp]=useState("");
+  let [previnp,setprevinp]=useState("");
+function changeInput(event){
+  setInp(event.target.value);
+}
+  function buttonClick(event){
+    event.preventDefault();
+    setInp("");
+    setprevinp(previnp+inp);
+  }
  
   return (
     <div>
    {/* the <p> tag with id text will show the concatinated string as output on string 
       the <input> tag with id input will take input entered by user */}
-  <input id='input' value={inputValue} onChange={changeInput} />
+  <input id='input' value={inp} onChange={changeInput} />
   
       <p id='intro'>Concated String</p>
-      <p id='text'>{text} </p>
+      <p id='text'>{previnp}</p>
       <button id='button' onClick={buttonClick}>
         Click me
       </button>
